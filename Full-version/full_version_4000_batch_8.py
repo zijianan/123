bcc30f0945cc3b55b8e51bc4a5fa08a06e1a3d73
zijianan <42871962+zijianan@users.noreply.github.com>
2019-04-30 22:28:35 -0400

    Update and rename full_version.py to full_version_4000_batch_8.py

--- Full-version/full_version_4000_batch_8.py
+++ Full-version/full_version_4000_batch_8.py
@@ -98,7 +98,7 @@ datagen = ImageDataGenerator(
         horizontal_flip=True)
 
 #Generate training data
-batch_size = 10
+batch_size = 8
 
 def image_a_b_gen(batch_size):
     for batch in datagen.flow(Xtrain, batch_size=batch_size):
@@ -113,7 +113,7 @@ def image_a_b_gen(batch_size):
 
 #Train model      
 model.compile(optimizer='adam', loss='mse')
-model.fit_generator(image_a_b_gen(batch_size), epochs=10, steps_per_epoch=1)
+model.fit_generator(image_a_b_gen(batch_size), epochs=4000, steps_per_epoch=1)
 
 
 # In[10]:
